c6ba024f68a6ec2da95a4cc14066c09f95a61d29
gongyho <gongyho@163.com>
2018-10-24 12:30:34 +0800

    solr搜索，关键字高亮显示

--- taotao-search/taotao-search-service/src/main/java/com/taotao/search/service/impl/SearchServiceImpl.java
+++ taotao-search/taotao-search-service/src/main/java/com/taotao/search/service/impl/SearchServiceImpl.java
@@ -1,14 +1,18 @@
 package com.taotao.search.service.impl;
 
 import java.util.List;
+import java.util.Map;
 
-import org.apache.solr.client.solrj.impl.HttpSolrClient;
-import org.apache.solr.common.SolrInputDocument;
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocumentList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.taotao.common.pojo.SearchItem;
+import com.taotao.common.pojo.SearchResult;
 import com.taotao.search.mapper.SearchMapper;
 import com.taotao.search.service.SearchService;
 
@@ -16,30 +20,91 @@ import com.taotao.search.service.SearchService;
 public class SearchServiceImpl implements SearchService {
 	@Autowired
 	private SearchMapper searchMapper;
-	@Value("${SOLR_BASE_URL}")
-	private String SOLR_BASE_URL;
-	
-	
+	@Autowired
+	private SolrClient solrClient;
+	@Value("${DEFAULT_COLLECTION}")
+	private String DEFAULT_COLLECTION;
+
 	public void importAllItem() throws Exception {
-		HttpSolrClient client=new HttpSolrClient.Builder(SOLR_BASE_URL).build();
-		//创建文档
-		SolrInputDocument document=null;
+		// 创建文档
 		List<SearchItem> list = searchMapper.searchAllItem();
-		for (SearchItem item : list) {
-			document =new SolrInputDocument();
-			//id域不能少
-			document.addField("id",item.getId());
-			document.addField("item_title",item.getTitle());
-			document.addField("item_sell_point",item.getSell_point());
-			document.addField("item_image", item.getImage());
-			document.addField("item_price",item.getPrice());
-			document.addField("item_category_name",item.getCategory_name());
-			document.addField("item_desc",item.getItem_desc());
-			client.add(document);
-		}
-		//提交
-		client.commit();
-		//关闭连接
-		client.close();
+		solrClient.addBeans(DEFAULT_COLLECTION,list);
+		// 提交
+		solrClient.commit(DEFAULT_COLLECTION);
+		// 关闭连接
+		solrClient.close();
 	}
+	/**
+	 * 搜索
+	 */
+	public SearchResult searchItem(String q, Integer page, Integer rows) throws Exception {
+		SolrQuery query = new SolrQuery();
+
+		query.setQuery(q);
+		query.setStart((page - 1) * rows);
+		query.setRows(rows);
+		query.set("df", "item_keywords");
+		//设置高亮
+		query.setHighlight(true);
+		query.setHighlightSimplePre("<em syle=\"color:red\">");
+		query.setHighlightSimplePost("</em>");
+		query.addHighlightField("item_title");
+		//搜索
+		QueryResponse queryResponse = solrClient.query(DEFAULT_COLLECTION,query);
+		Map<String, Map<String, List<String>>> highlighting = queryResponse.getHighlighting();
+		SolrDocumentList solrDocumentList = queryResponse.getResults();
+		List<SearchItem> list = queryResponse.getBeans(SearchItem.class);
+		//设置高亮
+		for (SearchItem item : list) {
+			List<String> titles = highlighting.get(item.getId()).get("item_title");
+			if(titles!=null && titles.size()>0) {
+				item.setTitle(titles.get(0));
+			}
+		}
+		
+		//设置
+		SearchResult result=new SearchResult();
+		result.setItemList(list);
+		result.setRecordCount(solrDocumentList.getNumFound());
+		Long pageCount = result.getRecordCount()/rows;
+		result.setPageCount(result.getRecordCount()%rows ==0 ? pageCount : pageCount+1);
+		
+		return result;
+	}
+	/*
+	private SearchResult search(SolrQuery query) throws Exception {
+		SearchResult searchResult = new SearchResult();
+		QueryResponse queryResponse = solrClient.query(query);
+		SolrDocumentList results = queryResponse.getResults();
+
+		// 设置总记录数
+		searchResult.setRecordCount(results.getNumFound());
+		List<SearchItem> itemList = new ArrayList<SearchItem>();
+		SearchItem item = null;
+
+		// 取高亮
+		Map<String, Map<String, List<String>>> highlighting = queryResponse.getHighlighting();
+		for (SolrDocument doc : results) {
+			item = new SearchItem();
+			item.setId(Long.valueOf(doc.get("id").toString()));
+			item.setImage(doc.get("item_image").toString());
+			item.setPrice(Long.valueOf(doc.get("item_price").toString()));
+			item.setSell_point(doc.get("item_sell_point").toString());
+			item.setCategory_name(doc.get("item_category_name").toString());
+
+			// 判断高亮
+			List<String> strs = highlighting.get(doc.get("id").toString()).get("item_title");
+			if (strs != null && strs.size() > 0) {
+				item.setTitle(strs.get(0));
+			} else {
+				item.setTitle(doc.get("item_title").toString());
+			}
+
+			itemList.add(item);
+		}
+		searchResult.setItemList(itemList);
+		solrClient.close();
+		return searchResult;
+	}
+	*/
 }
--- taotao-search/taotao-search-service/src/test/java/com/taotao/test/SolrjTest.java
+++ taotao-search/taotao-search-service/src/test/java/com/taotao/test/SolrjTest.java
@@ -1,12 +1,111 @@
 package com.taotao.test;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.CloudSolrClient;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.impl.HttpSolrClient.Builder;
+import org.apache.solr.client.solrj.response.ClusteringResponse;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocumentList;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.taotao.common.pojo.SearchItem;
 
 public class SolrjTest {
 	@Test
 	public void solrjTest() {
-		SolrClient client =new HttpSolrClient.Builder("http://192.168.37.145:8080/solr/core-taotao").build();
+		/*Builder builder = new HttpSolrClient.Builder("http://192.168.37.145:8080/solr/core-taotao");
+		builder.build();*/
+		ApplicationContext context=new ClassPathXmlApplicationContext("classpath:spring/applicationContext-solr.xml");
+		context.getBean("solrClient");
+	/*	Builder buider =(Builder) context.getBean("clientBuilder");
+		SolrClient client = buider.build();
+		SolrClient c =(SolrClient) context.getBean("solrClient");*/
+	}
+	
+	@Test
+	public void solrjQueryTest() throws SolrServerException, IOException {
+		//ApplicationContext context=new ClassPathXmlApplicationContext("classpath:spring/applicationContext-solr.xml");
+		/*Builder b =new HttpSolrClient.Builder("http://192.168.37.145:8080/solr/core-taotao");
+		SolrClient c=b.build();
+		SolrQuery query=new SolrQuery();
+		query.setQuery("阿尔卡特");
+		query.add("df","item_title");
+		query.add("wt","json");
+		QueryResponse query2 = c.query(query);
+		SolrDocumentList results = query2.getResults();
+		for (SolrDocument solrDocument : results) {
+			System.out.println(solrDocument.get("item_title"));
+		}*/
+		//System.out.println();
+		Long a=6L;
+		Long b=4L;
+		System.out.println(a%b);
+	}
+	
+	@Test
+	public void hightTest() throws SolrServerException, IOException {
+		Builder b =new HttpSolrClient.Builder("http://192.168.37.145:8080/solr/core-taotao");
+		SolrClient c=b.build();
+		
+		
+		SolrQuery query = new SolrQuery();
+
+		query.setQuery("手机");
+		query.set("df", "item_keywords");
+		//设置高亮
+		query.setHighlight(true);
+		query.setHighlightSimplePre("<em syle=\"color:red\">");
+		query.setHighlightSimplePost("</em>");
+		query.addHighlightField("item_title");
+		
+		QueryResponse query2 = c.query(query);
+		Map<String, Map<String, List<String>>> highlighting = query2.getHighlighting();
+		//System.out.println(highlighting);
+		for(String id1:highlighting.keySet()) {
+			String string = highlighting.get(id1).get("item_title").get(0);
+			System.out.println(string);
+			//System.out.println(id1);
+		}
+	}
+	
+	@Test
+	public void testSoleCloud() throws IOException, SolrServerException {
+		
+		
+		
+		/*List<String> strs=new ArrayList<String>();
+		strs.add("http://yhoo.fun:8180/solr");
+		strs.add("http://yhoo.fun:8280/solr");
+		strs.add("http://yhoo.fun:8380/solr");
+		strs.add("http://yhoo.fun:8480/solr");*/
+		ApplicationContext context=new ClassPathXmlApplicationContext("classpath:spring/applicationContext-solr.xml");
+		SolrClient client =(SolrClient) context.getBean("solrClient");
+		//SolrClient client=new CloudSolrClient.Builder(strs).build();
+		//SearchItem item=new SearchItem();
+		/*item.setId(123L);
+		item.setCategory_name("中国");
+		item.setImage("asd");
+		item.setTitle("中国");
+		item.setPrice(123L);
+		client.addBean("collection",item);
+		client.commit("collection");
+		client.close();*/
+		SolrQuery query=new SolrQuery();
+		query.setQuery("中国");
+		query.set("df", "item_title");
+		QueryResponse query2 = client.query("collection",query);
+
+		List<SearchItem> beans = query2.getBeans(SearchItem.class);
+		System.out.println(beans.get(0).getId());
 	}
 }
